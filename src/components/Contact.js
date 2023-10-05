c7be25c0b615766e8c30feaeab35595c54c507e5
Sarah Jacobs <sgjacobs14@gmail.com>
2023-10-05 15:23:02 -0400

    added contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 function Contact() {
   return (
     <div className="contact-page">
       <h2>Contact Me</h2>
-      
       <div className="social-icons">
-        <a href="mailto:sgjacobs14@gmail.com">
-          <i className="far fa-envelope"></i> Email Me
+      <a href="https://www.linkedin.com/in/sarahgjacobs/" target="_blank" rel="noopener noreferrer" style={{ marginRight: '20px' }}>
+        <i className="fab fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin"></i> LinkedIn
-        </a>
-        <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i> GitHub
-        </a>
+        <a href="https://github.com/sarahgjacobs" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-github fa-2x"></i></a>
       </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
